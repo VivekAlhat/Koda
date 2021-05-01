55a8c40781455d17f5771d5b67d20e1664ee7f6d
VivekAlhat <vivekalhat14@outlook.com>
2021-05-01 16:46:54 +0530

    Create Cart Methods

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,31 @@
 import create from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
-const store = (set) => ({
+const store = (set, get) => ({
   cart: [],
-  addToCart: (id) => console.log(id),
+  addToCart: (id) => {
+    let cart = get().cart;
+    if (!cart.find((item) => item.itemId === id)) {
+      set((state) => ({
+        cart: [...state.cart, { itemId: id, quantity: 1 }],
+      }));
+    } else {
+      set((state) => {
+        state.cart[state.cart.findIndex((item) => item.itemId === id)]
+          .quantity++;
+      });
+    }
+  },
+  removeFromCart: (id) => {
+    set((state) => ({
+      cart: [state.cart.filter((item) => item.itemId !== id)],
+    }));
+  },
+  clearCart: () => {
+    set((state) => ({
+      cart: [],
+    }));
+  },
 });
 
 const useStore = create(devtools(persist(store)));
