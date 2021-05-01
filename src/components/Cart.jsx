9a0f197e06a40cbc52a673f238b644d4da975c3e
VivekAlhat <vivekalhat14@outlook.com>
2021-05-02 02:14:25 +0530

    Create Cart Item Render Method

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,27 +1,10 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import useStore from "../store/store";
 import "./css/cart.css";
 
 const Cart = () => {
   const cart = useStore((state) => state.cart);
-  const [cartItems, setCartItems] = useState([]);
 
-  useEffect(() => {
-    cart.forEach((item) => {
-      const { itemId, quantity } = item;
-      axios
-        .get(`https://fakestoreapi.com/products/${itemId}`)
-        .then((res) =>
-          setCartItems((prev) => {
-            return [...prev, { ...res.data, quantity: quantity }];
-          })
-        )
-        .catch((err) => console.log(err));
-    });
-  }, [cart]);
-
-  return <div className="cart">{console.log(cartItems)}</div>;
+  return <div className="cart">{console.log(cart)}</div>;
 };
 
 export default Cart;
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,27 @@
+import axios from "axios";
 import create from "zustand";
 import { devtools, persist } from "zustand/middleware";
+
+const fetchData = (itemId) => {
+  return axios
+    .get(`https://fakestoreapi.com/products/${itemId}`)
+    .then((res) => {
+      return res;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 const store = (set, get) => ({
   cart: [],
   addToCart: (id) => {
     let cart = get().cart;
+    let product = fetchData(id);
+
     if (!cart.find((item) => item.itemId === id)) {
       set((state) => ({
-        cart: [...state.cart, { itemId: id, quantity: 1 }],
+        cart: [...state.cart, { product: product, itemId: id, quantity: 1 }],
       }));
     } else {
       set((state) => {
